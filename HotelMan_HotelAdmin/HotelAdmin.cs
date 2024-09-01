fcc7e8230d568454461680f6eb5b22a959e02f9f
Sang Nguyen <sangndtk@gmail.com>
2024-09-01 18:43:49 +0800

    v1

---
+++ HotelMan_HotelAdmin/HotelAdmin.cs
@@ -0,0 +1,30 @@
+﻿using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.SystemTextJson;
+
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace HotelMan_HotelAdmin
+{
+    public class HotelAdmin
+    {
+        public APIGatewayProxyResponse AddHotel(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            var response = new APIGatewayProxyResponse
+            {
+                Headers = new Dictionary<string, string>(),
+                StatusCode = 200
+            };
+
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+            response.Headers.Add("Access-Control-Allow-Headers", "*");
+            response.Headers.Add("Access-Control-Allow-Methods", "OPTIONS,POST");
+            response.Headers.Add("Content-Type", "application/json");
+
+            Console.WriteLine("OK.");
+
+            return response;
+        }
+    }
+}
